06622de8a78eea23bb9c7cbff18edcb9fec3e226
barence <44985626+barence@users.noreply.github.com>
2024-11-18 12:06:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+class Staff{
+    constructor(name, title, location, email){
+        this.name = name;
+        this.title = title;
+        this.location = location;
+        this.email = email;
+    }
+    viewStaff(){
+        console.log(this.name);
+        console.log(this.title);
+        console.log(this.location);
+        console.log(this.email);
+        name1.textContent = this.name;
+    }
+}
+const Jimmy = new Staff("Jimmy Wess","Principle","Big Office","ThisIsMy@gmail.com"); 
+Jimmy.viewStaff();
